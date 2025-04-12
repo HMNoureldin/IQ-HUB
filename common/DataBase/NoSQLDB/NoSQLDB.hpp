9897f0595d803ce2b97234fa603c9e499943ce6f
H.Noureldin <h.noureldin@super-b.com>
2025-04-12 20:35:28 +0200

    add NoSQLDB && SQLiteDB impl

---
+++ common/DataBase/NoSQLDB/NoSQLDB.hpp
@@ -0,0 +1,12 @@
+#ifndef NO_SQL_DB_H
+#define NO_SQL_DB_H
+
+
+#include <string>
+#include <vector>
+#include <map>
+
+#include "IDatabase.hpp"
+
+
+#endif // NO_SQL_DB_H
---
+++ common/DataBase/SQLiteDB/SQLiteDB.cpp
@@ -0,0 +1,63 @@
+#include "SQLiteDB.hpp"
+#include <iostream>
+#include <sstream>
+
+SQLiteDB::SQLiteDB(const std::string& dbPath) : 
+    dbPath_(dbPath),
+    db_(nullptr) {
+
+}
+
+bool SQLiteDB::open() {
+    int rc = sqlite3_open(dbPath_.c_str(), &db_);
+    if (rc != SQLITE_OK) {
+        lastError_ = sqlite3_errmsg(db_);
+        db_ = nullptr;
+        return false;
+    }
+    return true;
+}
+
+void SQLiteDB::close() {
+    if (db_) {
+        sqlite3_close(db_);
+        db_ = nullptr;
+    }
+}
+
+bool SQLiteDB::execute(const std::string& sql, std::string& result) {
+    char* errMsg = nullptr;
+    result.clear(); // Clear the result string before execution
+
+    // If the SQL is a SELECT statement, collect the results.
+    int rc = sqlite3_exec(db_, sql.c_str(), callback, &result, &errMsg);
+
+    if (rc != SQLITE_OK) {
+        lastError_ = errMsg;
+        sqlite3_free(errMsg);
+        return false;
+    }
+
+    return true;
+}
+
+
+int SQLiteDB::callback(void* data, int argc, char** argv, char** azColName) {
+    std::string* result = static_cast<std::string*>(data);
+
+    // Format each row of the SELECT result as a string.
+    std::stringstream row;
+    for (int i = 0; i < argc; ++i) {
+        row << azColName[i] << ": " << (argv[i] ? argv[i] : "NULL") << " | ";
+    }
+    row << "\n";
+
+    // Append the row to the result string.
+    *result += row.str();
+
+    return 0;
+}
+
+std::string SQLiteDB::getLastError() const {
+    return lastError_;
+}
