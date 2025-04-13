9d8e69579eb5e17cd917c94563194306279a9220
H.Noureldin <h.noureldin@super-b.com>
2025-04-13 13:06:57 +0200

    add IPC D_BUS handler

---
+++ KVPStorage/CommHandlers/IPCDBusHandler/IPCDBusHandler.cpp
@@ -0,0 +1,36 @@
+#include <csignal>
+#include <atomic>
+#include <thread>
+#include "IPCDBusHandler.hpp"
+
+std::atomic<bool> running = true;
+
+
+static void catchExitSignals() {
+    auto sigHandler = [](int){
+      running = false;
+    };
+  
+    /* Catch signals SIGINT and SIGTERM */
+    if (signal(SIGINT, sigHandler) == SIG_ERR || 
+        signal(SIGTERM, sigHandler) == SIG_ERR) {
+      LOG_ERROR << "Failed to set up signal handler for SIGINT or SIGTERM" << std::endl;
+      exit(EXIT_FAILURE);
+    }
+}
+
+
+void IPCDBusHandler::run(DataStore& store) 
+{
+    catchExitSignals();
+
+    while (running ){
+
+        // Simulate some work or wait
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    }
+
+    LOG_WARN << "IPCDBusHandler stopped" << std::endl;
+
+}
